852c0ea6454b9b73696125dbaae6e61627021efc
Ajeet Shah <ajeets.shah@hotmail.com>
2020-04-20 18:52:57 +0530

    Move autoclose config to env file

--- template/src/app/app.tsx
+++ template/src/app/app.tsx
@@ -7,10 +7,11 @@ import { ToastContainer } from 'react-toastify'
 
 export default function App() {
   const loading = useSelector(selectLoading)
+  const toastDuration = process.env.REACT_APP_TOAST_DURATION || 5000
 
   return (
     <Suspense fallback={<ReLoader loading={true} />}>
-      <ToastContainer autoClose={5000} toastClassName="f-14" />
+      <ToastContainer autoClose={toastDuration} toastClassName="f-14" />
       <ReLoader loading={loading} />
       <Routes />
     </Suspense>

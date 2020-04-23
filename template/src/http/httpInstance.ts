1bff110758ec0df6ef3d0eb6321bb9b9af79e7a7
Ajeet Shah <ajeets.shah@hotmail.com>
2020-04-23 13:19:32 +0530

    API timeout -> 20 seconds

--- template/src/http/httpInstance.ts
+++ template/src/http/httpInstance.ts
@@ -8,7 +8,7 @@ import {
 
 const host = process.env.REACT_APP_API_HOST
 const port = process.env.REACT_APP_API_PORT
-const timeout = Number(process.env.REACT_APP_API_TIMEOUT || 2000)
+const timeout = Number(process.env.REACT_APP_API_TIMEOUT || 20000)
 
 const instance = axios.create({
   baseURL: `//${host}:${port}`,
